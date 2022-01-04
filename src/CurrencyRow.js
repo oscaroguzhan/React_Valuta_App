820209bfcbce8e29f4a5679800cabc94d6c0ace1
oscaroguzhan <72919227+oscaroguzhan@users.noreply.github.com>
2022-01-04 16:02:04 +0100

    initial setup

---
+++ src/CurrencyRow.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const CurrencyRow = () => {
+    return (
+        <main>
+        
+            <input type="number" className='input'/>
+            <select className='select'>
+                <option value="Hi">
+                    Hi
+                </option>
+            </select>
+        </main>
+    )
+}
+
+export default CurrencyRow
